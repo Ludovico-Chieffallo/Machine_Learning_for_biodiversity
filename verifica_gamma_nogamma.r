e7b70e64dc4e25a5a1212fa6d6ff4ae025f5750a
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2023-03-28 17:21:26 +0200

    Update verifica_gamma_nogamma.r

--- verifica_gamma_nogamma.r
+++ verifica_gamma_nogamma.r
@@ -1,11 +1,19 @@
-library(raster)
 install.packages("rasterVis")
+
+library(raster)
 library(rasterVis)
 try<-raster("D:/Desktop/prova gamma.tif")
 try1<-raster("D:/Desktop/no gamma.tif")
 
 image(try)
 levelplot(try, 
+          col.regions = rev(terrain.colors(255)),
+          at = seq(0, 255, length = 256),
+          colorkey = list(space = "bottom", 
+                          width = 1, height = 0.5, 
+                          labels=list(at=seq(0, 255, by=50))),
+          margin = FALSE)
+levelplot(try1, 
           col.regions = rev(terrain.colors(255)),
           at = seq(0, 255, length = 256),
           colorkey = list(space = "bottom", 
